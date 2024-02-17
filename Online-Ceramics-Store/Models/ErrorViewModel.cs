fdc7490e16aac6dc385513b0b4cc96b32cb6f188
osnatshabtay <osnatshabtay98@gmail.com>
2024-02-17 19:00:25 +0200

    first commit

---
+++ Online-Ceramics-Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Ceramics_Store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
