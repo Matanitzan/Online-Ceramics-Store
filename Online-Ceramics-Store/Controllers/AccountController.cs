b77f6ba6964dfa219c7e618f8883125caabbeab0
jonatanbuga <yhonabo@ac.sce.ac.il>
2024-03-02 19:15:09 +0200

    create 2 pages Login and Register . connect bettween them to model: customer

---
+++ Online-Ceramics-Store/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Online_Ceramics_Store.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Online_Ceramics_Store.Controllers
+{
+    public class Account : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Login")]
+        public IActionResult Login() {
+            return View();
+        }
+
+        [Route("RegisterCustomer")]
+        public IActionResult RegisterCustomer()
+        {
+            Customer customer = new Customer();
+            return View("RegisterCustomer",customer);
+        }
+
+        [Route("AddCustomer")]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("showDetails", customer);
+            }
+            else {
+                return View("RegisterCustomer", customer);
+            }
+
+        }
+
+    }
+}
+
---
+++ Online-Ceramics-Store/Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Online_Ceramics_Store.Models
+{
+	public class Customer
+	{
+		public Customer()
+		{
+		}
+		public int cust_id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Full name must be between 2-20 letters")]
+        public string full_name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "mail must be with @ in the middle")]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "password must be between 2-20 letters")]
+        public string password { get; set; }
+
+
+        [Required]
+        [RegularExpression(@"^05\d{8}$", ErrorMessage = "Please enter a valid phone number")]
+        public string phone { get; set; }
+
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "city must be between 2-20 letters")]
+        public string city { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "address must be between 4-40 letters")]
+        public string address { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
+
