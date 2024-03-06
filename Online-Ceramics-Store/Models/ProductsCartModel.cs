c3ac3f2fce80f391785f50037772abd0b70a9a59
Matan Nitzan <1234matani@gmail.com>
2024-03-06 09:45:18 +0200

    new productcart class

---
+++ Online-Ceramics-Store/Models/ProductsCartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Online_Ceramics_Store.Models
+{
+	public class ProductsCartModel
+	{
+        public int? userID { get; set; } = null;
+        Dictionary <int, int> productsDetailCart = new Dictionary<int, int>(); //key=itemID, value=quantity per item
+        public ProductsCartModel()
+		{
+        }
+    }
+}
+
