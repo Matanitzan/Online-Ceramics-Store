e4a953df7fe24facb057bc61fbde06233687e10b
Tal-Elzam <Talelzam555@gmail.com>
2024-03-23 16:58:54 +0200

    Adding information to the product model and changing the details page

--- Online-Ceramics-Store/Models/Product.cs
+++ Online-Ceramics-Store/Models/Product.cs
@@ -10,11 +10,14 @@ namespace Online_Ceramics_Store.Models
 		public int item_id { get; set; }
 		public string name { get; set; }
         public string description { get; set; }
+        public string information { get; set; }
+
         public int stock_quantity { get; set; }
         public int category_id { get; set; }
         public int insale { get; set; }
         public int percent { get; set; }
         public int price { get; set; }
+
 
 	}
 }
